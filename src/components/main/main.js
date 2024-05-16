e12a846d50135c227302e33d9bafc2137fda6af2
geronilton <geronilton001@gmail.com>
2024-05-15 22:08:30 -0300

    criei componente main

---
+++ src/components/main/main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './main.css'
+import Nav from '../navcontent/navcontent'
+
+const main = () => {
+    return(
+        <main>
+            <Nav />
+        </main>
+    )
+}
+
+export default main;
