95304b5b09598f27b82a7ca3817de4b2bc77a1cd
geronilton <geronilton001@gmail.com>
2024-05-15 22:09:35 -0300

    componente header

---
+++ src/components/header/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './header.css'
+
+const header = () =>{
+    return(
+    <header>
+        <form className="formSearch">
+            <input className="inputSearch" 
+            type="text" name="search" 
+            title="Pesquisar" placeholder="Pesquisar uma tarefa" ></input>
+            <button className="buttonSearch" type="submit">Pesquisar</button>
+        </form>
+    </header>
+    );
+};
+
+export default header;
