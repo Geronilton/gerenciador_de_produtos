e008ab02ee24064e6fd1838dbfa5a3e01072b972
geronilton <geronilton001@gmail.com>
2024-05-16 22:27:37 -0300

    criei local de criar uma tarefa

---
+++ src/components/main/criartarefa/criartarefa.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './criartarefa.css'
+
+const criartarefa = () =>{
+    return(
+    <div className="criarTarefa">
+        <div className="tarefa">
+            <h2 className="subtitle">Criar uma tarefa</h2>
+            <form>
+                <input className="titleTarefa" type="text" placeholder="Adicione um titulo"/>
+                <br />
+                <br />
+                <textarea className="desTarefa" type="text" placeholder="Adicione uma descrição"/>
+                <br />
+                <button className="submit">Criar</button>
+            </form>
+        </div>
+    </div>
+    )
+}
+
+export default criartarefa;
