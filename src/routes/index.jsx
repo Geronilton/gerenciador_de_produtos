ea3ccfdbc3a6811bb977baf709318d2306b15e1b
geronilton <geronilton001@gmail.com>
2024-06-03 22:43:38 -0300

    rota main privado por login

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from '../components/login/login';
 import Main from  '../components/main/main'
+import Cadastro from '../components/cadastro/cadastro'
+import PrivateRoutes from './privateRoutes'
 
 export const AppRouter = () => {
     return (
         <Router>
-            <Routes> 
-                <Route path="/" element={<Main />} />
+            <Routes>
+                <Route path='/' element={<PrivateRoutes />} >
+                    <Route path="/" element={<Main />} />
+                </Route>
                 <Route path="/login" element={<Login />} />
+                <Route path="/cadastro" element={<Cadastro/>} />
                
             </Routes>
         </Router>
