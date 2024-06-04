28b8134766348f8dca8c00a0de6a8155ce1c5455
geronilton <geronilton001@gmail.com>
2024-06-03 22:41:54 -0300

    tornando todas as rotas privadas por login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
 import {AppRouter} from './routes/index'
+import { AuthProvider } from './context/auth';
 
  function App() {
   return (
     <div className="App">
-      <AppRouter></AppRouter>
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
     </div>
   );
 }
