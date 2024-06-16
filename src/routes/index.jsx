d0a5cceb531bf252c447403dc4b0a83823444b55
geronilton <geronilton001@gmail.com>
2024-06-16 00:01:47 -0300

    rotas dos novos componentes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,17 +3,26 @@ import Login from '../components/login/login';
 import Main from  '../components/main/main'
 import Cadastro from '../components/cadastro/cadastro'
 import PrivateRoutes from './privateRoutes'
+import CriarProduto from '../components/main/criarProduto/criarProduto'
+import ListagemProdutos from '../components/main/listagemProdutos/ListagemProdutos';
+import Estoque from '../components/main/estoque/Estoque';
+// import EditarProduto from '../components/main/editarProduto/EditarProduto';
 
 export const AppRouter = () => {
     return (
         <Router>
             <Routes>
-                <Route path='/' element={<PrivateRoutes />} >
-                    <Route path="/" element={<Main />} />
+                <Route path='/' element={<PrivateRoutes />}>
+                    <Route path="/" element={<Main />}>
+                        <Route path="criarProduto" element={<CriarProduto />} />
+                        <Route path="listagemProdutos" element={<ListagemProdutos />} />
+                        <Route path="estoque" element={<Estoque />} />
+                        {/* <Route path='editarProduto' element={<EditarProduto/>} /> */}
+                    </Route>
                 </Route>
+
                 <Route path="/login" element={<Login />} />
-                <Route path="/cadastro" element={<Cadastro/>} />
-               
+                <Route path="/cadastro" element={<Cadastro />} />
             </Routes>
         </Router>
     );
