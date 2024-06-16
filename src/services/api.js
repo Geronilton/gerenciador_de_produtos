720f38086e7debcc76c12f44048efc283f9419fc
geronilton <geronilton001@gmail.com>
2024-06-16 00:01:20 -0300

    configuração da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,14 @@
 import axios from "axios";
 
-export const api = new axios.create({
+const api = new axios.create({
     baseURL: "http://localhost:3001",
-}); 
+});
+
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem('@Auth:token'); // Atualizado para o prefixo usado no AuthProvider
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+export {api};
